315b1eb2928e24bd006668a739f59049ccf4843b
TheSeagullDev <67653706+TheSeagullDev@users.noreply.github.com>
2024-07-12 19:49:38 -0400

    Create computer choice function

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-console.log('Hello World!');
+function getComputerChoice() {
+    switch (Math.floor(Math.random() * 3)) {
+        case 0:
+            return "rock";
+        case 1:
+            return "paper";
+        case 2:
+            return "scissors"
+        }
+}
+
+console.log(getComputerChoice());
